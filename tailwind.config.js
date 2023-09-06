63dd35bff38a6e965352167b1d9ca3714668d3aa
Tamara Lazerka <lazerkatamara@gmail.com>
2023-09-06 14:29:22 -0700

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "15px",
+      },
+      colors: {
+        accent: "#08E95E",
+      },},
+  },
+  plugins: [],
+}
+
